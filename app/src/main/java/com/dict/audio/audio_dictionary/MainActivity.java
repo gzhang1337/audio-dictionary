6eb294abb489549fba731fcd7ec6a4101835c0d0
Gary Zhang <yzhang713@gmail.com>
2015-11-20 16:30:43 -0500

    added layouts and some activity code

--- app/src/main/java/com/dict/audio/audio_dictionary/MainActivity.java
+++ app/src/main/java/com/dict/audio/audio_dictionary/MainActivity.java
@@ -1,16 +1,40 @@
 package com.dict.audio.audio_dictionary;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.main);
+        Button login = (Button) findViewById(R.id.login_button);
+        Button signup = (Button) findViewById(R.id.signup_button);
+        login.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        TextView userId = (TextView) findViewById(R.id.userId);
+                        TextView pw = (TextView) findViewById(R.id.password);
+                        //TODO authenticate user and start acitviity based on results of authetnitcation
+                    }
+                }
+        );
+        signup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this,SignUpActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 
     @Override
